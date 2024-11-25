f541088cdb63d59a8e27afa74df3d0497f85aaa2
João Lucas <133472451+BlackWingJL@users.noreply.github.com>
2024-11-25 08:18:42 -0300

    AnalisadorCompleto 0.0.1
    klkl

--- Index.js
+++ Index.js
@@ -1,185 +1,180 @@
-var FileSystem = require('file-system');
+const FileSystem = require('file-system');
 
-const Delimitadores = require('./Linguagem/Delimitadores.js');
 const operadores = require('./Linguagem/Operadores.js');
+const Delimitadores = require('./Linguagem/Delimitadores.js');
 const PalavrasReservadas = require('./Linguagem/PalavrasReservadas.js');
 
 const code = FileSystem.readFileSync('./Data/InputCode.c', 'utf8');
+let cvsFile = 'Token; Lexema; Descrição\n'; // Cabeçalho do CSV
 
 let trail = "nada";
 let current_token = "";
 let $VerifyBlock = "";
 let commentLine = false;
 let commentBlock = false;
-let backslash = false;
+let backslash = true;
 let open_quotes = false;
 let type_open_quotes = "";
-let cvsFile = '';
 
 for (let char of code) {
     if (open_quotes) {
         current_token += char;
 
-        if (!backslash && char == type_open_quotes) {
-            open_quotes = !open_quotes;
+        if (!backslash && char === type_open_quotes) {
+            open_quotes = false;
             trail = "literal";
             current_token = checkToken(current_token);
         }
-        if (!backslash && char == '\\')
-            backslash = true;
-        else
-            backslash = false;
 
-        trail = "Delimitadores";
+        backslash = char === '\\' && !backslash;
     } else if (commentBlock) {
-        if (char == "*") {
-            $VerifyBlock = "*";
-        } else {
-            $VerifyBlock += char;
-        }
-
         current_token += char;
-        if ($VerifyBlock == '*/') {
+
+        if ($VerifyBlock === '*' && char === '/') {
             commentBlock = false;
             trail = "comentario";
             current_token = checkToken(current_token);
         }
+        $VerifyBlock = char === '*' ? '*' : "";
     } else if (commentLine) {
-        if (char == "\n" || char == "\r") {
+        if (char === '\n' || char === '\r') {
             commentLine = false;
             trail = "comentario";
             current_token = checkToken(current_token);
         } else {
             current_token += char;
         }
-    } else if (char == "\n" || char == "\r") {
-        // Ignore newline and carriage return
-    } else {
-        if (!is_space(char)) {
+    } else if (char === '\n' || char === '\r') {
+        // Ignorar novas linhas sem processar
+        current_token = checkToken(current_token);
+    } else if (!is_space(char)) {
 
-            if (!is_operator(char)) {
+        if (!is_operator(char)) {
 
-                if (!is_delimiter(char)) {
+            if (!is_delimiter(char)) {
 
-                    if (!is_number(char)) {
-                        if (char == "." && is_number(current_token)) {
-                            current_token += char;
-                            trail = "constante";
-                        } else {
-                            if (trail != "letra")
-                                current_token = checkToken(current_token);
-
-                            current_token += char;
-                            trail = "letra";
-                        }
-                    } else if (is_number(current_token[0]) && is_number(char)) {
-                        current_token += char;
+                if (!is_number(char)) {
+                    if (char === '.' && is_number(current_token)) {
+                        current_token += char; // Continua construindo o número decimal
                         trail = "constante";
                     } else {
-                        current_token = checkToken(current_token);
-                        current_token += char;
-                        trail = "constante";
+                        if (trail !== "letra") 
+                            current_token = checkToken(current_token); // Processa o token anterior
+
+                        current_token += char; // Continua construindo o identificador
+                        trail = "letra";
                     }
                 } else {
-                    if (char == '"' || char == '\'') {
-                        type_open_quotes = char;
-                        current_token = char;
-                        open_quotes = !open_quotes;
-                    } else {
-                        current_token = checkToken(current_token);
-                        current_token += char;
-                        trail = "delimitador";
-                        current_token = checkToken(current_token);
-                    }
+                    current_token += char; // Continua construindo um número
+                    trail = "constante";
                 }
-            } else if (operadores.includes(current_token[0])) {
-                current_token += char;
-                trail = "operador";
-                if (is_commentLine())
-                    commentLine = true;
-                if (is_commentBlock())
-                    commentBlock = true;
             } else {
-                current_token = checkToken(current_token);
-                current_token += char;
-                trail = "operador";
+                current_token = checkToken(current_token); // Processa o token anterior
+                current_token += char; // Trata o delimitador
+                trail = "delimitador";
+                current_token = checkToken(current_token); // Processa o delimitador imediatamente
             }
         } else {
-            current_token = checkToken(current_token);
-            trail = "nada";
+            current_token = checkToken(current_token); // Processa o token anterior
+            current_token += char; // Trata o operador
+            trail = "operador";
         }
+    } else {
+        current_token = checkToken(current_token); // Processa o token ao encontrar espaço
+        trail = "nada";
     }
+}
+if (current_token != '')
+    checkToken(current_token);
 
-    if (current_token != '')
-        checkToken(current_token);
+// Escreve os tokens acumulados no arquivo apenas uma vez, no final
+FileSystem.writeFileSync('./Data/OutputTable.csv', cvsFile);
 
-    FileSystem.writeFileSync('./Data/OutputTable.csv', cvsFile, function (err) { });
+function checkToken(current_token) {
+    current_token = current_token.trim(); // Remove espaços e quebras de linha extras
+    if (current_token !== '') {
+        switch (trail) {
+            case "comentario":
+                insertTable(current_token, 'Comentário');
+                break;
 
-    function checkToken(current_token) {
-        if (current_token !== '') {
-            switch (trail) {
-                case "comentario":
-                    insertTable(current_token, 'Comentário');
-                    break;
-                case "letra":
-                    if (PalavrasReservadas.includes(current_token)) {
-                        insertTable(current_token, 'Palavra Reservada');
-                    } else {
-                        insertTable(current_token, 'Identificador');
-                    }
-                    break;
-                case "constante":
-                    insertTable(current_token, 'Constante');
-                    break;
-                case "operador":
+            case "letra":
+                if (PalavrasReservadas.includes(current_token)) {
+                    insertTable(current_token, 'Palavra Reservada');
+                } else if (current_token != "\n") {
+                    insertTable(current_token, 'Identificador');
+                }
+                break;
+
+            case "operador":
+                if (current_token == '=')
+                    insertTable(current_token, 'Atribuição');
+                else
                     insertTable(current_token, 'Operador');
-                    break;
-                case "delimitador":
+                break;
+
+            case "constante":
+                insertTable(current_token, 'Constante Numérica');
+                break;
+
+            case "literal":
+                insertTable(current_token, 'Constante Literal');
+                break;
+
+            case "delimitador":
+                if (current_token === ',')
+                    insertTable(current_token, 'Separador');
+                else if (current_token === ';')
+                    insertTable(current_token, 'Terminador');
+                else if (['(', '{', '['].includes(current_token))
+                    insertTable(current_token, 'Delimitador - Abertura');
+                else if ([')', '}', ']'].includes(current_token))
+                    insertTable(current_token, 'Delimitador - Fechamento');
+                else
                     insertTable(current_token, 'Delimitador');
-                    break;
-                default:
-                    break;
-            }
-        }
-        return '';
-    }
-
-    function insertTable(id, token_type) {
-        var token = "";
-        switch (token_type) {
-            case 'Identificador':
-            case 'Constante Numérica':
-            case 'Constante Literal':
-                token = `<${token_type}, ${id}>`;
-                break;
-
-            case 'comentário':
-                token = `<${token_type}, ${id}>`;
-                id = 'comentário';
                 break;
         }
+    }
+    return ''; // Reseta o token atual
+}
 
-        cvsFile += `${token};\t ${id};\t ${token_type}\n`;
+function insertTable(id, token_type) {
+    var token = "";
+    let descricao = token_type;
+
+    switch(token_type) {
+        case 'Identificador':
+        case 'Constante Numérica':
+        case 'Constante Literal':
+            token = `<${token_type}, ${id}>`;
+            break;
+
+        case 'Comentário':
+            token = `<${token_type}, ${id}>`;
+            descricao = 'Comentário'; // Para comentários, usamos uma descrição simples
+            break;
+
+        default:
+            token = `<${id}, >`;
+            break;
     }
 
-    function is_space(char) {
-        return char === ' ';
-    }
+    cvsFile += `${token};\t ${id};\t ${descricao}\n`;
+}
 
-    function is_commentLine() {
-        return current_token === '//';
-    }
+function is_space(char) {
+    return char === ' ';
+}
 
-    function is_operator(char) {
-        return operadores.includes(char);
-    }
+function is_operator(char) {
+    return operadores.includes(char);
+}
 
-    function is_delimiter(char) {
-        return Delimitadores.includes(char);
-    }
+function is_delimiter(char) {
+    return Delimitadores.includes(char);
+}
 
-    function is_number(char) {
-        var n = parseInt(char);
-        return Number.isInteger(n);
-    }
+function is_number(char) {
+    var n = parseInt(char);
+    return Number.isInteger(n);
 }
